379bbd3d627ea805b17687627dc0cf2040859af1
abidjamali <muhammadkhanjam065@gmail.com>
2025-12-12 18:41:14 +0500

    Fix: Enabled AuthProvider wrapper in main.jsx to resolve useAuth crash

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,21 @@
+// client/src/main.jsx - FINAL CORRECT CODE
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; 
 import { BrowserRouter } from 'react-router-dom';
 
-// Zaroori: Agar aap AuthContext use kar rahe hain, toh isko import karein
-// import { AuthProvider } from './context/AuthContext.jsx'; // <--- Agar aapne yeh file banayi hai toh uncomment karein
+// ZAROORI: AuthProvider import ko uncomment karein
+import { AuthProvider } from './context/AuthContext.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Agar AuthProvider hai toh usse wrap karein, nahi toh sirf BrowserRouter rakhein */}
-    {/* <AuthProvider> */} 
+    {/* ZAROORI: AuthProvider se App ko wrap karein */}
+    <AuthProvider>  
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    {/* </AuthProvider> */}
+    </AuthProvider>
   </React.StrictMode>
 );
