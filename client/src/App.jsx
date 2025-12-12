cabe5e459d4f6842e0980e2277cf7e84550ebaac
abidjamali <muhammadkhanjam065@gmail.com>
2025-12-12 18:37:44 +0500

    Fix: Final App.jsx correction to fix runtime crash (Removed duplicate Router, added main tag, uncommented HomePage)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// BrowserRouter ko yahan se hata diya gaya hai, sirf Routes aur Route hain
+import { Routes, Route } from 'react-router-dom'; 
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-// Import your pages here
-// import HomePage from './pages/HomePage'; 
+
+// Zaroori: HomePage component ko uncomment kar dein
+import HomePage from './pages/HomePage'; 
+// (Agar aapke paas aur pages hain toh unko bhi yahan import karein, jaise Login/Register)
+
 
 function App() {
   return (
-    <Router>
+    // <> fragment ka use kiya gaya hai kyunki Router main.jsx mein hai
+    <> 
       <Navbar />
-      <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        {/* Add your other routes here */}
-      </Routes>
+      <main> {/* main tag mein poora content wrap kar dein for index.css to work */}
+        <Routes>
+          {/* Default Home Page route */}
+          <Route path="/" element={<HomePage />} /> 
+          
+          {/* Aapke doosre routes yahan add honge, jaise: */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* <Route path="/create" element={<CreatePostPage />} /> */}
+          
+        </Routes>
+      </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
