8d0ac9088d93b27532a7f08f2d6faed39899d71f
abidjamali <muhammadkhanjam065@gmail.com>
2025-12-13 09:40:53 +0500

    Fix: Added start.sh script to fix Railway monorepo deployment

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# server directory mein jaao
+cd server
+
+# Server ki dependencies install karo
+npm install
+
+# Server ko start karo (Yeh maan kar chal rahe hain ki server/package.json mein 'start' script hai)
+npm start
